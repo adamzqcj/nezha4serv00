22a45fe49b0241318d282e61df8ee4e1c50ad07d
adamzqcj <adamzqcj@gmail.com>
2025-01-26 21:53:48 +0800

    Update install-agent.sh

--- install-agent.sh
+++ install-agent.sh
@@ -5,8 +5,8 @@ WORKDIR="/home/${USERNAME}/.nezha-agent"
 
 # 下载 nezha-agent 的函数
 download_agent() {
-    #DOWNLOAD_LINK="https://github.com/nezhahq/agent/releases/latest/download/nezha-agent_freebsd_amd64.zip"
-    DOWNLOAD_LINK="https://github.com/nezhahq/agent/releases/download/v1.1.4/nezha-agent_freebsd_amd64.zip"
+    DOWNLOAD_LINK="https://github.com/nezhahq/agent/releases/latest/download/nezha-agent_freebsd_amd64.zip"
+    #DOWNLOAD_LINK="https://github.com/nezhahq/agent/releases/download/v1.1.4/nezha-agent_freebsd_amd64.zip"
     if ! wget -qO "$ZIP_FILE" "$DOWNLOAD_LINK"; then
         echo 'error: Download failed! Please check your network or try again.'
         return 1
